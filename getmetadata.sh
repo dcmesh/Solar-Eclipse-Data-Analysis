573affac1ddd427989c8e26211211e4ad26534f0
jxe162 <joannaelia258@gmail.com>
2021-04-14 13:15:29 -0400

    remove duplicate lines

--- getmetadata.sh
+++ getmetadata.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #need to have spec.txt in same folder as bash script
-cat spec.txt > metadata.csv
-find . -name "*.wav" | while read filename; do mediainfo --Output=file://spec.txt $filename >> metadata.csv; done;
+cat spec.txt > temp
+find . -name "*.wav" | while read filename; do mediainfo --Output=file://spec.txt $filename >> temp; done;
+echo "$(awk '!a[$1]++' temp)" > metadata.csv
+rm temp
