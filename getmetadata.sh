4459fdff76e3ddddd279326797e53fd5205870e1
jxe162 <joannaelia258@gmail.com>
2021-06-30 15:52:08 -0400

    cleans up label and adds comments

--- getmetadata.sh
+++ getmetadata.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-#need to have spec.txt in same folder as bash script
-cat spec.txt > temp
+# need to have spec.txt in same folder as bash script
+
+# adds labels
+cat spec.txt | sed 's/%//g' | sed 's/^.*;//g' > temp
+
+# finds all wav files and gets metadata from
 find . -name "*.wav" | while read filename; do mediainfo --Output=file://spec.txt $filename >> temp; done;
+
+# removes duplicates
 echo "$(awk '!a[$1]++' temp)" > metadata.csv
+
 rm temp
